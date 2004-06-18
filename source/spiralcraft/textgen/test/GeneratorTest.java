aa237eee358abb5092503486209646abfaa381cd
mike <>
2004-06-18 21:18:32 +0000

    tests

--- source/spiralcraft/textgen/test/GeneratorTest.java
+++ source/spiralcraft/textgen/test/GeneratorTest.java
@@ -14,45 +14,56 @@ import java.io.Writer;
 
 import spiralcraft.time.Clock;
 
+import spiralcraft.exec.Executable;
+import spiralcraft.exec.ExecutionContext;
+import spiralcraft.exec.ExecutionTargetException;
+
 /**
  * Parser for text generation markup language.
  */
 public class GeneratorTest
+  implements Executable
 {
-  public static void main(String[] args)
-    throws Exception
+  public void execute(ExecutionContext context,String[] args)
+    throws ExecutionTargetException
   {
-    InputStream in
-      =GeneratorTest.class.getResourceAsStream("generatorTest.textgen");
-    
-    String content=new String(StreamUtil.readBytes(in));
-    in.close();
-
-    Generator generator=new Generator(content);
-    generator.bind(new BeanFocus(new GeneratorTest()));
-    
-    Writer writer=new OutputStreamWriter(System.out);
-    generator.write(writer);
-    writer.flush();
-
-    Clock clock=Clock.instance();
-    long time=System.currentTimeMillis();
-    long duration=5000;
-
-    StringWriter stringWriter=new StringWriter();
-
-    long iterations=0;
-    while (true)
-    { 
-      generator.write(stringWriter);
-      stringWriter.getBuffer().setLength(0);
-      iterations++;
-      if (clock.approxTimeMillis()-time>duration)
-      { break;
+    try
+    {
+      InputStream in
+        =GeneratorTest.class.getResourceAsStream("generatorTest.textgen");
+      
+      String content=new String(StreamUtil.readBytes(in));
+      in.close();
+  
+      Generator generator=new Generator(content);
+      generator.bind(new BeanFocus(new GeneratorTest()));
+      
+      Writer writer=new OutputStreamWriter(ExecutionContext.getInstance().out());
+      generator.write(writer);
+      writer.flush();
+  
+      Clock clock=Clock.instance();
+      long time=System.currentTimeMillis();
+      long duration=5000;
+  
+      StringWriter stringWriter=new StringWriter();
+  
+      long iterations=0;
+      while (true)
+      { 
+        generator.write(stringWriter);
+        stringWriter.getBuffer().setLength(0);
+        iterations++;
+        if (clock.approxTimeMillis()-time>duration)
+        { break;
+        }
       }
+     
+      context.out().println("rate="+(iterations) / ((System.currentTimeMillis()-time)/(float) 1000));
     }
-   
-    System.out.println("rate="+(iterations) / ((System.currentTimeMillis()-time)/(float) 1000));
+    catch (Exception x)
+    { throw new ExecutionTargetException(x);
+    }
     
   }
   
