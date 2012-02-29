ba54bf6143815bfd356c7b4dce98c6c5b5cf9ce4
mike <>
2012-02-29 00:54:50 +0000

    PrepareMessage has a singleton instance to reduce the             amount of generated garbage.

--- source/spiralcraft/textgen/PrepareMessage.java
+++ source/spiralcraft/textgen/PrepareMessage.java
@@ -26,6 +26,7 @@ public class PrepareMessage
     extends Message
 {
   public static final Type TYPE=new Type();
+  public static final PrepareMessage INSTANCE=new PrepareMessage();  
   
   { multicast=true;
   }
